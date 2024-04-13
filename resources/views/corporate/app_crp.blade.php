24834d28288d59bdbe500dd68d00379c343bf99b
MANOJ GORE <manojgore.mg@gmail.com>
2024-04-13 08:49:12 +0530

    tg

--- resources/views/corporate/app_crp.blade.php
+++ resources/views/corporate/app_crp.blade.php
@@ -109,7 +109,7 @@ $g_setting = \App\Models\GeneralSetting::where('id',1)->first();
                         </a>
                     </li>
 
-                    <div class="topbar-divider d-none d-sm-block"></div>
+                    <div class="topbar-divider d-none d-sm-block"></div> 
                     <!-- Nav Item - User Information -->
                     <li class="nav-item dropdown no-arrow">
                         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
